4e26881f79ebeec5252c8ef095189e607254186e
avega2991 <rambler2991@rambler.ru>
2015-04-29 14:36:38 +0500

    a_little_fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ EdxFive::Application.routes.draw do
   root :to => "main#index"
 
   get '/main', :to => "main#index"
+  get '/main/index', :to => "main#index"
 
   get 'main/search', :to => "main#search"
   post '/main/search', :to => "main#search"
